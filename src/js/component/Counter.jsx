1cf64127a442adad1d385202da9aabd2b83e47bb
Jose Hurtado <hello@hurtadojose.com>
2022-07-05 23:10:28 +0000

    Counter Exercise Using React

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Counter(props) {
+ 	return <div className="container">
+    <h1>SIMPLE COUNTER</h1>
+    <div className="container-lg row">
+        <div className="col box">
+        <h2 class="fa-regular fa-clock"></h2>
+        </div>
+        <div className="col box">
+        <h2>0</h2>
+        </div>
+        <div className="col box">
+        <h2>0</h2>
+        </div>
+        <div className="col box">
+        <h2>0</h2>
+        </div>
+        <div className="col box">
+        <h2>0</h2>
+        </div>
+        <div className="col box">
+        <h2>0</h2>
+        </div>
+    </div>
+</div>
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+import Counter from "./component/Counter.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
